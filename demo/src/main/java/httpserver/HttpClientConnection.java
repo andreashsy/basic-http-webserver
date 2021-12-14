b65f068507e02de71d160fe8023e6762778ccf20
andreashsy <songyanghan+git@gmail.com>
2021-12-14 14:40:18 +0800

    saving

--- demo/src/main/java/httpserver/HttpClientConnection.java
+++ demo/src/main/java/httpserver/HttpClientConnection.java
@@ -33,8 +33,11 @@ public class HttpClientConnection implements Runnable {
             DataInputStream dis = new DataInputStream(bis);
             BufferedOutputStream bos = new BufferedOutputStream(os);
             DataOutputStream dos = new DataOutputStream(bos);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            String line = dis.readUTF();
+            //String line = dis.readUTF();
+            String line = in.readLine();
             System.out.println("Client said: " + line);
             if (line.indexOf(" ") == -1) {
                 System.out.println("Invalid Argument");
@@ -52,8 +55,9 @@ public class HttpClientConnection implements Runnable {
             
             // if not a GET method
             if (!(methodName.equals("GET"))) {
-                dos.writeUTF("HTTP/1.1 405 Method Not Allowed \\r\\n\\r\\n " + methodName + " not supported \\r\\n");
-                dos.flush();
+                out.write("HTTP/1.1 405 Method Not Allowed\r\n");
+                out.write("\r\n");
+                out.write(methodName + " not supported \\r\\n");
                 System.out.println("Method not supported, closing connection...");
                 socket.close();
             }
@@ -62,15 +66,16 @@ public class HttpClientConnection implements Runnable {
             List<String> res = new ArrayList<String>();
             for (String resource:resources) {
                 String[] rl = resource.split("/");
-                String ele = rl[rl.length -1];
+                String ele = "/" + rl[rl.length -1];
                 res.add(ele);
             }
             System.out.println("Resource names: " + res);
 
             // if client requests a resource thats not found
             if (!(res.contains(resourceName))) {
-                dos.writeUTF("HTTP/1.1 404 Not Found \\r\\n\\r\\n " + resourceName + " not found \\r\\n");
-                dos.flush();
+                out.write("HTTP/1.1 404 Not Found\r\n");
+                out.write("\r\n");
+                out.write(resourceName + " not found \\r\\n");
                 System.out.println("Resource not found, closing connection...");
                 socket.close();
             }
@@ -88,22 +93,33 @@ public class HttpClientConnection implements Runnable {
 
            
             // if resource is PNG
-            if (resourceName.endsWith("\\.png")) {
+            if (resourceName.endsWith("png")) {
                 byte[] filecontent = Files.readAllBytes(targetResourcePath);
                 System.out.println("Sending PNG...");
-                dos.writeUTF("HTTP/1.1 200 OK \\r\\n Content-Type: image/png \\r\\n\\r\\n");
-                dos.flush();
+                out.write("HTTP/1.1 200 OK \r\n");
+                out.write("Content-Type: image/png \\r\\n");
+                out.write("\r\n");                     
                 os.write(filecontent, 0, filecontent.length);
                 os.flush();
                
             // if resource is not PNG
             } else {
-                byte[] filecontent = Files.readAllBytes(targetResourcePath);
+                File file = new File(targetResourcePath.toString());
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                // byte[] filecontent = Files.readAllBytes(targetResourcePath);
                 System.out.println("Sending file...");
-                dos.writeUTF("HTTP/1.1 200 OK \\r\\n\\r\\n");
-                dos.flush();
-                os.write(filecontent, 0, filecontent.length);
-                os.flush();
+                out.write("HTTP/1.1 200 OK \r\n");
+                out.write("\r\n");
+                String l = reader.readLine();
+                while (l != null) {
+                    printWriter.println(line);// print current line
+                    line = reader.readLine();// read next line
+                }
+                reader.close();
+                printWriter.close();
+                //os.write(filecontent, 0, filecontent.length);
+                //os.flush();
             } 
 
             
